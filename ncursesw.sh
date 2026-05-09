e2bf807ceeba9417b8f54de48dfd268944f7af83
DaisyTheDev <DaisyTheFoxxo@proton.me>
2026-05-09 12:41:33 -0500

    ncursesw: Change install directory to be more consistent

--- ncursesw.sh
+++ ncursesw.sh
@@ -4,18 +4,18 @@ if [ -f "./ncursesw.tar.zst" ]; then
   exit
 fi
 
-rm -rf ./ncurses-inst
-mkdir -p ./ncurses-inst
-mkdir -p ./ncurses-inst/usr/bin
-mkdir -p ./ncurses-inst/usr/sbin
-mkdir -p ./ncurses-inst/usr/lib
-mkdir -p ./ncurses-inst/usr/lib32
-mkdir -p ./ncurses-inst/usr/lib64
-ln -s usr/bin ./ncurses-inst/bin
-ln -s usr/sbin ./ncurses-inst/sbin
-ln -s usr/lib ./ncurses-inst/lib
-ln -s usr/lib32 ./ncurses-inst/lib32
-ln -s usr/lib64 ./ncurses-inst/lib64
+rm -rf ./ncursesw-inst
+mkdir -p ./ncursesw-inst
+mkdir -p ./ncursesw-inst/usr/bin
+mkdir -p ./ncursesw-inst/usr/sbin
+mkdir -p ./ncursesw-inst/usr/lib
+mkdir -p ./ncursesw-inst/usr/lib32
+mkdir -p ./ncursesw-inst/usr/lib64
+ln -s usr/bin ./ncursesw-inst/bin
+ln -s usr/sbin ./ncursesw-inst/sbin
+ln -s usr/lib ./ncursesw-inst/lib
+ln -s usr/lib32 ./ncursesw-inst/lib32
+ln -s usr/lib64 ./ncursesw-inst/lib64
 
 cd ncurses
 
@@ -27,9 +27,9 @@ set -e
 cd build
 CC=/usr/local/bin/clang CXX=/usr/local/bin/clang++ LD=/usr/local/bin/ld.lld CFLAGS="-g0 -O2" CPPFLAGS="-g0 -O2" ../configure --prefix=/usr --enable-widec --with-shared --without-debug --without-tests --without-ada
 make -j$(nproc --all)
-sudo make install DESTDIR=$(realpath ../../ncurses-inst)
+sudo make install DESTDIR=$(realpath ../../ncursesw-inst)
 
-cd ../../ncurses-inst
+cd ../../ncursesw-inst
 rm -f ./bin
 rm -f ./sbin
 rm -f ./lib
@@ -38,4 +38,4 @@ rm -f ./lib64
 sudo find . -type d -empty -delete
 tar pmcfv - . | zstd -22 --ultra > ../ncursesw.tar.zst
 cd ..
-sudo rm -rf ncurses-inst
+sudo rm -rf ncursesw-inst
